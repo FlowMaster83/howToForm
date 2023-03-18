6eeed947793e72d6149d085f7185d850af88e389
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2023-03-18 14:33:23 +0200

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,23 +1,33 @@
+import { Field, Formik, Form } from 'formik';
+
+const initialValues = {
+  login: '123',
+  password: 'qwe',
+};
+
 export const LoginForm = () => {
-    const handleSubmit = e => {
-        e.preventDefault();
-        const { login, password } = e.target.elements;
-        console.log(login.value, password.value);
-    }
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    console.log(actions);
+  };
 
   return (
-    <form autoComplete="off" onSubmit={handleSubmit}>
-      <label htmlForm="login">
-        Login
-        <input type="text" name="login" />
-      </label>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form autoComplete="off">
+        <label htmlForm="login">
+          Login
+          {/* значение атрибута name (login) должен совпадать с объектом initialValues */}
+          <Field type="text" name="login" />
+        </label>
 
-      <label htmlForm="password">
-        Password
-        <input type="password" name="password" />
-      </label>
-      
-      <button type="submit">Submit</button>
-    </form>
+        <label htmlForm="password">
+          Password
+          {/* значение атрибута name (password) должен совпадать с объектом initialValues */}
+          <Field type="password" name="password" />
+        </label>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
